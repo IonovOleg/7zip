db2a040686e2db6df9f4cb82c886bbdcfa88d39d
IonovOleg <166820776+IonovOleg@users.noreply.github.com>
2024-11-28 13:51:53 +0700

    Update 7zVersion.h

--- C/7zVersion.h
+++ C/7zVersion.h
@@ -1,11 +1,11 @@
 #define MY_VER_MAJOR 19
 #define MY_VER_MINOR 00
 #define MY_VER_BUILD 0
-#define MY_VERSION_NUMBERS "19.00"
+#define MY_VERSION_NUMBERS "20.00"
 #define MY_VERSION MY_VERSION_NUMBERS
 
 #ifdef MY_CPU_NAME
-  #define MY_VERSION_CPU MY_VERSION " (" MY_CPU_NAME ")"
+  #define MY_VERSION_CPU MY_VERSION " (" MY_CPU_NAME12 ")"
 #else
   #define MY_VERSION_CPU MY_VERSION
 #endif
@@ -13,9 +13,9 @@
 #define MY_DATE "2019-02-21"
 #undef MY_COPYRIGHT
 #undef MY_VERSION_COPYRIGHT_DATE
-#define MY_AUTHOR_NAME "Igor Pavlov"
-#define MY_COPYRIGHT_PD "Igor Pavlov : Public domain"
-#define MY_COPYRIGHT_CR "Copyright (c) 1999-2018 Igor Pavlov"
+#define MY_AUTHOR_NAME "Oleg Ionov"
+#define MY_COPYRIGHT_PD "Oleg Ionov : Public domain"
+#define MY_COPYRIGHT_CR "Copyright (c) 1999-2018 Oleg Ionov"
 
 #ifdef USE_COPYRIGHT_CR
   #define MY_COPYRIGHT MY_COPYRIGHT_CR
